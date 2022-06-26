840f87c82a8c23c6e811ec94004c498fedcaea8d
Senshiben-efrei <ayman.ben.hajjaj@efrei.net>
2022-06-26 06:45:19 +0200

    figma design completed checkpoint before implementing authstack

---
+++ Stacks/AuthStack.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Signin from '../components/Signup';
+
+export default function Profile() {
+  return (
+    <View>
+      <Signin/>
+    </View>
+  );
+}
